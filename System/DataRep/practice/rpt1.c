0df49ce6a2b5c96df9b44c9ed3525f8d297717e0
redzzzi <myqueen1346@naver.com>
2024-04-29 00:25:22 +0900

    dir for byte rep repeat

---
+++ System/DataRep/practice/rpt1.c
@@ -0,0 +1,11 @@
+// code to Print Byte Representation of Data
+// casting pointer 'unsigned char *' allows treatment as a byte array
+
+typedef unsigned char *pointer;
+
+void show_bytes(pointer start, size_t len) {
+  size_t i;
+  for (i = 0; i < len; i++)
+    printf("%p\t0x%.2x\n", start+i, start[i]);
+  print("\n");
+}
